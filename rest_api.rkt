5a8a8ba86ec26af83ff7f609f724e7b49474b23a
Pjotr Prins <pjotr.public01@thebird.nl>
2019-06-30 13:06:44 +0000

    Using at-exp for SPARQL query and unpacking JSON

--- rest_api.rkt
+++ rest_api.rkt
@@ -1,4 +1,5 @@
-#lang racket
+#lang racket ; at-exp br
+
 (require (planet dmac/spin))
 
 (require net/url)
@@ -8,6 +9,15 @@
        (call/input-url (string->url "https://www.ifixit.com/api/2.0/guides/13470")
                        get-pure-port port->string)
        ))
+
+(define (sparql_homologeneid gene_name)
+  @string-append{
+A wonderful
+multiline string
+that goes on
+seemingly
+forever.})
+
 
 #|
 curl "http://localhost:8000/wikidata" returns
@@ -28,7 +38,7 @@ curl "http://localhost:8000/wikidata" returns
 
 (get "/wikidata"
      (lambda ()
-       (call/input-url (string->url "https://query.wikidata.org/sparql?query=SELECT %3FhomologeneID%0AWHERE%0A{%0A%20%20%20 %23 wd%3AQ18247422 corresponds to Add1 in house mouse%0A%20%20%20 wd%3AQ18247422 wdt%3AP593 %3FhomologeneID .%0A%20%20%20%20%0A%20%20%20 %23 an entire URI can be used%2C if it's wrapped in <>%0A%20%20%20 %23 <http%3A%2F%2Fwww.wikidata.org%2Fentity%2FQ29723729> wdt%3AP593 %3FhomologeneID%0A}%0A%0A")
+       (call/input-url (string->url "https://query.wikidata.org/sparql?query=SELECT %3FhomologeneID%0AWHERE%0A{%0A%20%20%20 %23 wd%3AQ18247422 corresponds to Add1 in house mouse%0A%20%20%20 wd%3AQ18247422 wdt%3AP593 %3FhomologeneID .%0A%20%20%20%20%0A%2019%20%20 %23 an entire URI can be used%2C if it's wrapped in <>%0A%20%20%20 %23 <http%3A%2F%2Fwww.wikidata.org%2Fentity%2FQ29723729> wdt%3AP593 %3FhomologeneID%0A}%0A%0A")
                        get-pure-port port->string)
      ))
 
--- test/protoype.rkt
+++ test/protoype.rkt
@@ -1,6 +1,7 @@
-#lang racket
+#lang at-exp racket
 (require json)
 (require net/url)
+(require net/uri-codec)
 
 (define (fetch url)
   (call/input-url (string->url url)
@@ -21,22 +22,47 @@
 
 ;;; header
 (define header 
- '("Accept: application/json"))
+  '("Accept: application/json"))
 
+(define sparql_homologene_id
+  @string-append{
+ SELECT ?homologeneID
+ WHERE
+ {
+  # wd:Q18247422 corresponds to Add1 in house mouse
+  wd:Q18247422 wdt:P593 ?homologeneID .
+    
+  # an entire URI can be used, if it's wrapped in <>
+  # <http://www.wikidata.org/entity/Q29723729> wdt:P593 ?homologeneID
+ }
 
-(define wikidata
-       (call/input-url (string->url "https://query.wikidata.org/sparql?query=SELECT %3FhomologeneID%0AWHERE%0A{%0A%20%20%20 %23 wd%3AQ18247422 corresponds to Add1 in house mouse%0A%20%20%20 wd%3AQ18247422 wdt%3AP593 %3FhomologeneID .%0A%20%20%20%20%0A%20%20%20 %23 an entire URI can be used%2C if it's wrapped in <>%0A%20%20%20 %23 <http%3A%2F%2Fwww.wikidata.org%2Fentity%2FQ29723729> wdt%3AP593 %3FhomologeneID%0A}%0A%0A")
-                       get-pure-port
-                       port->string
-                       header
-                       ))
+}
+  )
+
+(define wikidata-json
+  (call/input-url (string->url (string-append "https://query.wikidata.org/sparql?query=" (uri-encode sparql_homologene_id)))
+                  get-pure-port
+                  (lambda (port)
+                    (string->jsexpr (port->string port))
+                    )
+                  header
+                  ))
+
+(define wikidata-string
+  (call/input-url (string->url (string-append "https://query.wikidata.org/sparql?query=" (uri-encode sparql_homologene_id)))
+                  get-pure-port
+                  port->string
+                  header
+                  ))
 
 (require rackunit)
 
 (test-case
  "tests"
  (display "Running tests...")
- (display wikidata )
+ (display sparql_homologene_id)
+ (display wikidata-string)
+ 
  ; (check-equal? (fetch "http://localhost:8000/") '("Hello GeneNetwork3!"))
  ; (check-equal? (fetch "http://localhost:8000/gene/aliases/BRCA2") '("BRCA2"))
  ; (display (fetch-string "http://localhost:8000/wikidata") )
