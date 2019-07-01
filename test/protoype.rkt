2eb95dd41e8fb587bb5ae385c93ed546c83e102c
Pjotr Prins <pjotr.public01@thebird.nl>
2019-07-01 20:13:07 +0000

    Fetch wikidata-id

--- test/protoype.rkt
+++ test/protoype.rkt
@@ -26,33 +26,28 @@
 
 (define (sparql_wikidata_id gene_name)
   @string-append{
- SELECT DISTINCT ?geneid ?label
+ SELECT DISTINCT ?wikidata_id
  WHERE {
   # hint:Query hint:optimizer "None" .
-  ?geneid wdt:P31 wd:Q7187 .
-  ?s ps:P702 ?geneid .
-  ?geneid rdfs:label "BRCA1"@"@"en .
+  ?wikidata_id wdt:P31 wd:Q7187 .
+  ?s ps:P702 ?wikidata_id .
+  ?wikidata_id rdfs:label "@gene_name"@"@"en .
  }
 }
   )
                  
-(define sparql_homologene_id
+(define (sparql_homologene_id wikidata_id)
   @string-append{
  SELECT ?homologeneID
  WHERE
  {
-  # wd:Q18247422 corresponds to Add1 in house mouse
-  wd:Q18247422 wdt:P593 ?homologeneID .
-    
-  # an entire URI can be used, if it's wrapped in <>
-  # <http://www.wikidata.org/entity/Q29723729> wdt:P593 ?homologeneID
- }
+  <@~a{@|wikidata_id|}> wdt:P593 ?homologeneID .
+ } 
+  }
+)
 
-}
-  )
-
-(define wikidata-json
-  (call/input-url (string->url (string-append "https://query.wikidata.org/sparql?query=" (uri-encode sparql_homologene_id)))
+(define (wikidata-json query)
+  (call/input-url (string->url (string-append "https://query.wikidata.org/sparql?query=" (uri-encode query)))
                   get-pure-port
                   (lambda (port)
                     (string->jsexpr (port->string port))
@@ -67,6 +62,14 @@
                   header
                   ))
 
+(define (wikidata-values json)
+ 
+  (map (lambda (wikidata_id)
+         (hash-ref (hash-ref wikidata_id 'wikidata_id) 'value))
+     (hash-ref (hash-ref json 'results) 'bindings))
+       )
+  
+
 (require rackunit)
 
 (test-case
@@ -74,6 +77,19 @@
  (display "Running tests...")
  (display sparql_homologene_id)
  (display (wikidata-string (sparql_wikidata_id "BRCA2")))
+ (display (sparql_homologene_id "http://www.wikidata.org/entity/Q17853272"))
+ (let ([json (wikidata-json (sparql_wikidata_id "BRCA2"))])
+   (display json)
+   (let ([values (wikidata-values json)])
+     (display values)
+     (writeln "---")
+     (map (lambda (value)
+            (wikidata-string (sparql_homologene_id value)))
+            values))
+   
+   )
+ ; (display (wikidata-json (sparql_wikidata_id "BRCA2")))
+ 
           ; (check-equal? (fetch "http://localhost:8000/") '("Hello GeneNetwork3!"))
           ; (check-equal? (fetch "http://localhost:8000/gene/aliases/BRCA2") '("BRCA2"))
           ; (display (fetch-string "http://localhost:8000/wikidata") )
