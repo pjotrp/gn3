87af73952ce07e7e66674f83d04e927a11dd90c6
Pjotr Prins <pjotr.public01@thebird.nl>
2019-07-30 22:03:57 +0000

    Working on gene alias

--- gn3/web/wikidata.rkt
+++ gn3/web/wikidata.rkt
@@ -1,12 +1,16 @@
 #lang at-exp racket
 
 ;; Wikidata fetchers
+;;
+;; The end points use a simpile memoize routine which is reset by restarting the
+;; process.
 
-(provide wikidata-ids wikidata-gene-aliases gene-aliases)
+(provide sparql_gene_alias wikidata-ids wikidata-gene-aliases gene-aliases)
 
 (require json)
 (require net/url)
 (require net/uri-codec)
+(require memo)
 
 ;;; header
 (define header
@@ -21,7 +25,17 @@
 (define wd-gene "wd:Q7187")
 
 ;; Create a SPARQL query for fetching wikidata ids for a (case
-;; sensitive) gene name (Human Mouse and Rat)
+;; sensitive) gene name (in Human Mouse and Rat) e.g.
+#|
+SELECT DISTINCT ?wikidata_id
+       WHERE {
+         ?wikidata_id wdt:P31 wd:Q7187 ;
+                      wdt:P703 ?species .
+         VALUES (?species) { (wd:Q15978631) (wd:Q83310) (wd:Q184224) } .
+         ?wikidata_id rdfs:label "BRCA2"@en .
+         }
+|#
+
 (define (sparql_wikidata_id gene_name)
   @string-append{
      SELECT DISTINCT ?wikidata_id
@@ -34,7 +48,17 @@
             })
 
 ;; Create a SPARQL query for fetching aliases that go with a wikidata
-;; id
+;; id (within species) e.g. for Human BRCA2,
+;;
+#|
+SELECT DISTINCT ?alias
+       WHERE {
+               <http://www.wikidata.org/entity/Q17853272> rdfs:label ?name ;
+               skos:altLabel ?alias .
+               FILTER(LANG(?name) = "en" && LANG(?alias) = "en").
+             }
+|#
+
 (define (sparql_gene_alias wikidata_id)
   @string-append{
       SELECT DISTINCT ?alias
@@ -72,7 +96,8 @@
   )
 
 ;; Get the wikidata_ids from a gene Name
-(define (wikidata-ids gene-name)
+(define/memoize (wikidata-ids gene-name)
+  (display (sparql_wikidata_id gene-name))
   (let ([json (wikidata-json (sparql_wikidata_id gene-name))])
     (let ([values (wikidata-values json 'wikidata_id)])
       ; (display values)
@@ -80,7 +105,7 @@
       )))
 
 ;; Get gene aliases for a wikidata_id
-(define (wikidata-gene-aliases wikidata-id)
+(define/memoize (wikidata-gene-aliases wikidata-id)
   (let ([aliases
          (wikidata-values (wikidata-json (sparql_gene_alias
                                           wikidata-id)) 'alias)])
@@ -88,7 +113,7 @@
                 aliases)))
 
 ;; Get gene aliases for a gene name (human, mouse and rat)
-(define (gene-aliases gene-name)
+(define/memoize (gene-aliases gene-name)
   (let ([ids (wikidata-ids gene-name)])
      (flatten (map (lambda (id) (wikidata-gene-aliases id) )
          ids)))
--- test/wikidata-test.rkt
+++ test/wikidata-test.rkt
@@ -7,8 +7,12 @@
 
 (test-case
  "Wikidata-tests"
- (check-equal? (wikidata-ids "Shh") '("http://www.wikidata.org/entity/Q14860079" "http://www.wikidata.org/entity/Q24420953"))
+ (writeln "Running Wikidata-tests...")
+ (time (check-equal? (wikidata-ids "Shh") '("http://www.wikidata.org/entity/Q14860079" "http://www.wikidata.org/entity/Q24420953")))
+ (time (check-equal? (wikidata-ids "Shh") '("http://www.wikidata.org/entity/Q14860079" "http://www.wikidata.org/entity/Q24420953")))
+ ;; Test for human BRCA2
  (check-equal? (jsexpr->string (wikidata-ids "BRCA2"))  "[\"http://www.wikidata.org/entity/Q17853272\"]")
+ ; (display (sparql_gene_alias "http://www.wikidata.org/entity/Q17853272"))
  (check-equal? (wikidata-gene-aliases "http://www.wikidata.org/entity/Q17853272")
                '("FACD"
                  "FAD"
@@ -34,4 +38,6 @@
                  "XRCC11"))
  (check-equal? (gene-aliases "SHH")
                '("TPT" "HLP3" "HHG1" "HPE3" "MCOPCB5" "ShhNC" "SMMCI" "TPTPS"))
+ (check-equal? (gene-aliases "Shh") ;; returns mouse and rat!
+               '("Hx" "ShhNC" "9530036O11Rik" "Dsh" "Hhg1" "Hxl3" "M100081" "ShhNC")
  )
